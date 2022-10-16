8e8a896638f39ad95f4ea516a1a8dcc1cf3273eb
Adamya <adamyapathak12@gmail.com>
2022-10-16 08:15:19 -0400

    Add files via upload

---
+++ party.java
@@ -0,0 +1,12 @@
+public class party {
+    public static void main(String[] args) throws Exception {
+        System.out.println("And the winner of the SCRIW 2022 is, ...... Red Alliance team 9996 Cane Bay High School!!! ");
+        System.out.println("Special thanks to Adam, Ryan, Moustafa and all of the team members and mentors ");
+        System.out.println("Robot of the competition is...... SpongBot_SwervePants AKA ChampBot");
+        System.out.println("Congrats to everyone, Many more to come");
+        System.out.println("Our Hardwork Paid off");
+        
+
+
+    }
+}
